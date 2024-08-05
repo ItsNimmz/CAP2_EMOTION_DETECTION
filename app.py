143bd80457413d76f255c24dd85ce35622f5e8f6
nimisha <nimishanimmz@gmail.com>
2024-08-04 22:08:00 -0400

    for testing

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def detect_emotion_route():
         if 'image' not in request.files:
             return jsonify({'error': 'No image provided'}), 400
         x = request.files['image']
-        return jsonify({'Error--': x}), 200
+        return jsonify({'Error--'}), 200
 
         # image = np.frombuffer(x.read(), np.uint8)
         # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
