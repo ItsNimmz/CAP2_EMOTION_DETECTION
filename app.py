c768cfca848d50d1f55bf21802f352a4102ee5e2
nimisha <nimishanimmz@gmail.com>
2024-08-04 20:22:25 -0400

    for testing

--- app.py
+++ app.py
@@ -113,20 +113,19 @@ def detect_emotion_route():
         print('heree..............')
         if 'image' not in request.files:
             return jsonify({'error': 'No image provided'}), 400
-        # x = request.files['image']
-        return jsonify({'error----'}), 400
-        # image = np.frombuffer(x.read(), np.uint8)
-        # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        x = request.files['image']
+        image = np.frombuffer(x.read(), np.uint8)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-        # emotions = detect_emotion(image)
+        emotions = detect_emotion(image)
 
-        # if emotions:
-        #     return jsonify({'emotions': emotions}), 200
-        # else:
-        #     return jsonify({'error': 'No faces detected'}), 400
+        if emotions:
+            return jsonify({'emotions': emotions}), 200
+        else:
+            return jsonify({'error': 'No faces detected'}), 400
     except Exception as e:
         print(f'Error: {e}')
-        return jsonify({'error': 'Server error'}), 500
+        return jsonify({'Error: {e}': 'Server error'}), 500
 
 @app.route('/')
 def home():
